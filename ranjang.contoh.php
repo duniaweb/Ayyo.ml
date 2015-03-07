a9b194921f1ee2b85324f5f0c10d4c02e887265f
Baguz Ach <eggoez@users.noreply.github.com>
2015-03-07 07:39:51 +0700

    Create ranjang.contoh.php

---
+++ ranjang.contoh.php
@@ -0,0 +1,17 @@
+<?php
+define('S_REWRITEBASE','/');
+
+// Persiapan mau ML nya
+define('S_DB_HOST','localhost');
+define('S_DB_USER','my.user.db');
+define('S_DB_PASS','my.pass.db');
+define('S_DB_SLDB','my.name.db');
+
+define('S_SELFHOST',selfHost());
+define('S_MURL', S_SELFHOST.S_REWRITEBASE);
+define('S_TB_URLS','co_wo');
+define('S_TB_REFERERS','ce_we');
+define('S_TB_BLACKLIST','ow_rang');
+
+define('S_OPT_STATS',true);
+?>
